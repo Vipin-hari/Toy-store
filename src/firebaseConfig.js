49455d91d325b7214ea96f9a3f55e55b3f2efe98
Vipin-hari <84123617+Vipin-hari@users.noreply.github.com>
2024-08-09 19:56:59 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDLfP0w6PcBv2v0zvtb7LNdaEH3sFqF0_U",
+  apiKey: "CREATE YOUR OWN KEY",
   authDomain: "unknown-9d49f.firebaseapp.com",
   projectId: "unknown-9d49f",
   storageBucket: "unknown-9d49f.appspot.com",
   messagingSenderId: "160329090276",
-  appId: "1:160329090276:web:3e061a4d34c7e347b1d8f1",
+  appId: "CREATE YOUR OWN KEY",
   measurementId: "G-72DFQ5ML0M"
 };
 export default firebaseConfig;
